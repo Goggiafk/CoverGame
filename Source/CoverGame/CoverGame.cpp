08058c305b4fb14905d0e178c07e439b57e77833
Bogdan Panow <GoggiProsto@gmail.com>
2023-12-23 20:28:24 +0100

    Init Commit

---
+++ Source/CoverGame/CoverGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CoverGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CoverGame, "CoverGame" );
---
+++ Source/CoverGame/CoverGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CoverGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COVERGAME_API ACoverGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
